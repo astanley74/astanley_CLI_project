4187916c70c23e2c566f26518567bfd7ce159d24
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-08-21 22:25:47 -0400

    api setup

--- lib/astanley_CLI_project/api.rb
+++ lib/astanley_CLI_project/api.rb
@@ -1,3 +1,3 @@
 class AstanleyCLIProject::Api
-    url_1 = RestClient.get("https://api.sportradar.us/nhl/trial/v7/en/seasons/2019/REG/standings.json?api_key=y5eryabtncayc5db8x4m546p")
+    url_1 = RestClient.get("https://api.sportradar.us/nhl/trial/v7/en/seasons/2019/REG/standings.json?api_key=#{ENV['NHL_API_KEY]}")
 end
